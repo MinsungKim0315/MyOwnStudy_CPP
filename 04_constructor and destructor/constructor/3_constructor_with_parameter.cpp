dc962ea436360d7c51b6013ce57e4ebcc0167c8e
Minsung Kim <88697052+MinsungKim0315@users.noreply.github.com>
2024-06-03 17:01:04 +0900

    Create 3_constructor_with_parameter.cpp

---
+++ 04_constructor and destructor/constructor/3_constructor_with_parameter.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+class character {
+public:
+	character() : hp(100), power(100) {};
+protected:
+	int hp;
+	int power;
+};
+
+class player : public character {
+public:
+	player() {};
+};
+
+class monster {
+public:
+	monster() {};
+	void get_damage(int _damage) {};
+	void attack(player target_player) {};
+	void attack_special(player target_player);
+};
+
+void monster::attack_special(player target_player) {
+	cout << "basic attack: damage - 10 hp" << endl;
+}
+
+class monster_a :public monster, character {
+private:
+	int location[2];
+public:
+	monster_a() {
+		cout << "monster_a class's constructor" << endl;
+	};
+	monster_a(int x, int y) : location{ x, y } {	// Initializer List: initialize member variables
+		cout << "add a parameter to monster_a class's constructor" << endl;
+	};
+	void show_location() {
+		cout << "location: [" << location[0] << ", " << location[1] << "]" << endl;
+	};
+};
+
+int main() {
+	monster_a forest_monster;
+	forest_monster.show_location();
+	monster_a wood_monster(10, 25);
+	wood_monster.show_location();
+
+	return 0;
+}
